87cc43b288ea8f0bdab9a94ec1e5ee1d18bbd3ed
smmmfrd <smmmfrd@gmail.com>
2023-06-12 15:36:13 -0700

    Get Current Week's Sunday

--- cron/reset.ts
+++ cron/reset.ts
@@ -1,9 +1,28 @@
-import { PrismaClient, Movie } from "@prisma/client";
+import { PrismaClient, Movie, Theater, Showtime } from "@prisma/client";
 require("dotenv").config();
 
 const prisma = new PrismaClient();
 
 type TopMovies = [Movie, Movie, Movie, Movie, Movie, Movie, Movie, Movie];
+
+type Theaters = [
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater,
+  Theater
+];
 
 async function getMovies() {
   const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.MOVIE_DB_API_KEY}&page=1`;
@@ -30,18 +49,18 @@ async function buildTheaters(movies: TopMovies) {
   const indexToMovie = (index: number, even: boolean) => {
     // Should not have .title in final version.
     if (index % 2 == 0) {
-      return movies[0].title;
+      return movies[0];
     } else {
       if (index < 4) {
-        return movies[1].title;
+        return movies[1];
       } else if (index < 8) {
-        return movies[2].title;
+        return movies[2];
       } else if (index < 12) {
-        return movies[3].title;
+        return movies[3];
       } else if (index < 14) {
-        return movies[even ? 4 : 5].title;
+        return movies[even ? 4 : 5];
       } else {
-        return movies[even ? 6 : 7].title;
+        return movies[even ? 6 : 7];
       }
     }
   };
@@ -54,10 +73,10 @@ async function buildTheaters(movies: TopMovies) {
     for (let time = startTime; time < 24; time += 3) {
       even = !even;
       // Uncomment this
-      // showtimes.push({
-      //   time,
-      //   movie: indexToMovie(index, even),
-      // });
+      showtimes.push({
+        time,
+        movie: indexToMovie(index, even),
+      });
       showtimes.push(indexToMovie(index, even));
     }
 
@@ -72,16 +91,29 @@ async function buildTheaters(movies: TopMovies) {
 }
 
 async function reset() {
-  const movies = await getMovies();
-  // console.log(movies[0]);
+  const currentDate = new Date();
+  currentDate.setHours(10, 30, 0, 0);
+  const showDate = new Date(
+    currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 7)
+  );
+  console.log("Show Date:", showDate.toLocaleString());
 
-  await prisma.movie.deleteMany();
-  const create = await prisma.movie.create({
-    data: {
-      ...movies[0],
-    },
-  });
-  console.log(create);
+  // const movies = await getMovies();
+  // // console.log(movies[0]);
+  // const theater: Theater = {
+  //   theaterId: 0,
+  // };
+  // const showtimes: Showtime = {
+  //   time:
+  // }
+
+  // await prisma.movie.deleteMany();
+  // const create = await prisma.movie.create({
+  //   data: {
+  //     ...movies[0],
+  //   },
+  // });
+  // console.log(create);
 }
 
 reset();
