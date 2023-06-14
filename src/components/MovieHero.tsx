4079284dd63f08373198df660faa521916ec7d5e
smmmfrd <smmmfrd@gmail.com>
2023-06-14 12:48:54 -0700

    Movie Hero Refactor

---
+++ src/components/MovieHero.tsx
@@ -0,0 +1,38 @@
+import { Movie } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+
+type MovieHeroProps = {
+  movie: Movie;
+  altTitle?: string;
+};
+
+export default function MovieHero({ movie, altTitle = "" }: MovieHeroProps) {
+  return (
+    <header className="relative">
+      <div
+        style={
+          {
+            "--image-url": `url(${movie.backdropImage})`,
+          } as React.CSSProperties
+        }
+        className="h-72 w-full max-w-7xl bg-gray-500 bg-[image:var(--image-url)] bg-cover bg-center bg-no-repeat bg-blend-multiply"
+      ></div>
+      <figure className="">
+        <Image
+          className="absolute left-8 top-8 h-[288px] rounded"
+          src={movie.posterImage}
+          width={208}
+          height={288}
+          alt={`Poster for ${movie.title}`}
+        />
+        <figcaption className="mt-20 px-8">
+          <h2 className="text-3xl font-bold">
+            {altTitle.length > 0 ? altTitle : `${movie.title} at Fake Theater`}
+          </h2>
+          <p>{movie.description}</p>
+        </figcaption>
+      </figure>
+    </header>
+  );
+}
--- src/pages/movies/[movieId]/index.tsx
+++ src/pages/movies/[movieId]/index.tsx
@@ -9,12 +9,12 @@ import Head from "next/head";
 import Image from "next/image";
 import { ParsedUrlQuery } from "querystring";
 import React from "react";
+import MovieHero from "~/components/MovieHero";
 import ShowtimeCard from "~/components/ShowtimeCard";
 import { caller } from "~/server/api/root";
 
 // We need to convert our response to and from a string
-
-interface StaticPathParams extends ParsedUrlQuery {
+interface MovieStaticPathParams extends ParsedUrlQuery {
   movieId: string;
 }
 
@@ -32,7 +32,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<MoviePageProps> = async (
   context
 ) => {
-  const { movieId } = context.params as StaticPathParams;
+  const { movieId } = context.params as MovieStaticPathParams;
   const data = await caller.movies.getMovie({ movieId: +movieId });
   const movie: Movie = data.movie!;
   return {
@@ -54,31 +54,7 @@ const MoviePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       <Head>
         <title>{movie.title} | Fake Theater</title>
       </Head>
-      <header className="relative">
-        <div
-          style={
-            {
-              "--image-url": `url(${movie.backdropImage})`,
-            } as React.CSSProperties
-          }
-          className="h-72 w-full max-w-7xl bg-gray-500 bg-[image:var(--image-url)] bg-cover bg-center bg-no-repeat bg-blend-multiply"
-        ></div>
-        <figure className="">
-          <Image
-            className="absolute left-8 top-8 h-[288px] rounded"
-            src={movie.posterImage}
-            width={208}
-            height={288}
-            alt={`Poster for ${movie.title}`}
-          />
-          <figcaption className="mt-20 px-8">
-            <h2 className="text-3xl font-bold">
-              {movie.title} at Fake Theater
-            </h2>
-            <p>{movie.description}</p>
-          </figcaption>
-        </figure>
-      </header>
+      <MovieHero movie={movie} />
 
       <ShowtimeCard
         styleExtensions="mx-8 px-6 py-4 font-bold text-4xl rounded-lg"
