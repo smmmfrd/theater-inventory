de6f4ff00b93eceb978cec3a8eb2d026ec06d3af
smmmfrd <smmmfrd@gmail.com>
2023-06-22 11:55:48 -0700

    Orders Page

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,3 +1,4 @@
+import { type TicketOrder } from "@prisma/client";
 import { api } from "~/utils/api";
 
 export default function OrdersPage() {
@@ -7,5 +8,31 @@ export default function OrdersPage() {
     return <div>Loading</div>;
   }
 
-  return <div>There are: {query.data?.orders.length} orders</div>;
+  return (
+    <>
+      <header>
+        <h2>Ticket Orders</h2>
+      </header>
+
+      <section>
+        {query.data?.orders && <OrdersDisplay orders={query.data.orders} />}
+      </section>
+    </>
+  );
+}
+
+type OrdersDisplayType = {
+  orders: TicketOrder[];
+};
+
+function OrdersDisplay({ orders }: OrdersDisplayType) {
+  return <div className="flex flex-wrap gap-2">{orders.map(OrderCard)}</div>;
+}
+
+function OrderCard(order: TicketOrder) {
+  return (
+    <div className="rounded border-4 border-primary bg-primary">
+      {order.name}
+    </div>
+  );
 }
