d08b143ce724e6d7a07d0a167dbc2dfb607b0b9b
smmmfrd <smmmfrd@gmail.com>
2023-06-23 16:32:43 -0700

    Vercel Cron Job Test #6

--- src/pages/api/cron/ticketSales.ts
+++ src/pages/api/cron/ticketSales.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import { caller } from "~/server/api/root";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default async function handler() {
   const { showtimes } = await caller.showtimes.getAllShowtimes();
 
