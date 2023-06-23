beaa1df5944a7a1cb6fc6c808f948b4c4d24aa4b
smmmfrd <smmmfrd@gmail.com>
2023-06-23 16:20:02 -0700

    Vercel Cron Job Test #5

--- src/pages/api/cron/ticketSales.ts
+++ src/pages/api/cron/ticketSales.ts
@@ -1,8 +1,9 @@
+import { NextResponse } from "next/server";
 import { caller } from "~/server/api/root";
 
 export default async function handler() {
   const { showtimes } = await caller.showtimes.getAllShowtimes();
 
   console.log(showtimes.length);
-  return;
+  return NextResponse.json({ message: "fuck you" });
 }
