c6635f1d02b1818e9c02ab7b2875ec32702e48f3
smmmfrd <smmmfrd@gmail.com>
2023-06-24 23:34:27 -0700

    Vercel Cron Job Test #8

--- src/pages/api/cron/ticketSales.ts
+++ src/pages/api/cron/ticketSales.ts
@@ -1,29 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+// import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export const config = {
-  runtime: "edge",
-};
-
-export default async function handler() {
-  const prisma = new PrismaClient();
-  const showtimes = await prisma.showtime.findMany({
-    select: {
-      showtimeId: true,
-      time: true,
-      maxSeats: true,
-      movie: {
-        select: {
-          title: true,
-        },
-      },
-      tickets: true,
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-
-  console.log(showtimes.length);
-  return NextResponse.json({ message: "fuck you" });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ text: "fuck you vercel" });
 }
