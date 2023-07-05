11ae4a761882e00d171d607ec43c87c19cb675d9
smmmfrd <smmmfrd@gmail.com>
2023-07-05 11:24:02 -0700

    404 Page Responsive Styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ErrorPage() {
   return (
-    <div className="absolute left-4 top-1/3 flex items-center gap-6 p-2">
+    <div className="absolute left-4 top-1/3 flex items-center gap-6 p-2 sm:left-1/3 lg:top-16">
       <h2 className="text-3xl font-bold">404</h2>
       <div className="divider divider-horizontal"></div>
       <p className="flex flex-col gap-2 font-medium">
