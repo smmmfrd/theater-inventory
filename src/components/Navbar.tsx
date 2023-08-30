e36d91a9d04e05d8ce5a543a98d74e91ac3ffa75
smmmfrd <smmmfrd@gmail.com>
2023-08-30 13:01:14 -0700

    Navbar Text Style Fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ export default function Navbar() {
   const { cartTicketOrders } = useTicketStore();
 
   return (
-    <nav className="sticky top-0 z-10 -mb-5 h-14 bg-primary px-4 py-3">
+    <nav className="sticky top-0 z-10 -mb-5 h-14 bg-primary px-4 py-3 text-neutral">
       <div className="mx-auto flex max-w-3xl items-end justify-between">
         <Link href="/" className="link no-underline hover:underline">
-          <h1 className="text-2xl font-thin">Fake Theater</h1>
+          <h1 className="text-2xl">Fake Theater</h1>
         </Link>
         <ul className="flex gap-4">
           <li>
@@ -17,20 +17,20 @@ export default function Navbar() {
               className="link no-underline hover:underline"
               title="View and delete all orders by showtime"
             >
-              <h1 className="font-thin">Orders</h1>
+              <h2 className="">Orders</h2>
             </Link>
           </li>
           <li>
             <Link href="/about" className="link no-underline hover:underline">
-              <h1 className="font-thin">About</h1>
+              <h2 className="">About</h2>
             </Link>
           </li>
           <li>
             <Link href="/cart" className="link no-underline hover:underline">
-              <h1 className="font-thin">
+              <h2 className="">
                 Cart{" "}
                 {cartTicketOrders.length > 0 && `(${cartTicketOrders.length})`}
-              </h1>
+              </h2>
             </Link>
           </li>
         </ul>
