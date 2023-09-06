1041d0ee84ca35af2d3ff83066428927ae643bba
smmmfrd <smmmfrd@gmail.com>
2023-09-05 23:32:15 -0700

    Remove Outdated Rule

--- src/components/RegularLayout.tsx
+++ src/components/RegularLayout.tsx
@@ -6,7 +6,7 @@ type RegularLayoutProps = {
 
 export default function RegularLayout({ children }: RegularLayoutProps) {
   return (
-    <main className="bg-base relative flex min-h-screen flex-col items-center gap-5 pt-8">
+    <main className="bg-base relative flex min-h-screen flex-col items-center gap-5">
       {children}
     </main>
   );
